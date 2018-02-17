77196dd918eef3b5ce20c54ff192b35a0b312f91
Agnese Salutari <agneses92@hotmail.it>
2018-02-17 10:27:09 +0100

    English translation

--- mas/breastCancerAgBehaviour.pl
+++ mas/breastCancerAgBehaviour.pl
@@ -1,21 +1,24 @@
 danger([Id, 4], 1) :-
-    print('caso1: diagnosis=4'), nl, !.
+    print('case1: diagnosis=4'), nl, !.
 
 danger([Id, 2], 1) :-
     inHistory(Id, 'cancer'),
-    print('caso2: diagnosis=2 and cancer in history'), nl, !.
+    print('case2: diagnosis=2 and cancer in history'), nl, !.
 
 danger([Id, 0], 1) :-
     inHistory(Id, 'cancer'),
-    print('caso3: diagnosis=0 and cancer in hisotry'), nl, !.
+    print('case3: diagnosis=0 and cancer in hisotry'), nl, !.
 
 danger([Id, 2], 2) :-
     inDescription(Id, 'smoker'),
-    print('caso4: diagnosis=2 and smoker'), nl, !.
+    print('case4: diagnosis=2 and smoker'), nl, !.
 
 danger([Id, 0], 2) :-
     inDescription(Id, 'smoker'),
-    print('caso5: diagnosis=0 and smoker'), nl !.
+    print('case5: diagnosis=0 and smoker'), nl !.
 
 danger([Id, 2], 3) :-
-    print('caso6: diagnosis=2 and not cancer in history and not smoker'), nl, !.
+    print('case6: diagnosis=2 and not cancer in history and not smoker'), nl, !.
+
+danger([Id, 0], 3) :-
+    print('case7: diagnosis=0 and not cancer in history and not smoker'), nl, !.
